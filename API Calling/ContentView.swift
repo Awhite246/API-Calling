3c766b50a742b07774d411f5d4a14e5b76dc58bd
Awhite246 <awhite@bsd220.org>
2022-02-07 10:30:57 -0600

    :)

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -4,14 +4,45 @@
 //
 //  Created by Student on 2/3/22.
 //
+//https://community-open-weather-map.p.rapidapi.com/weather?q=Barrington,IL,US&units=imperial&rapidapi-key=cc6b0c5350msh5a3d7cf2c2a71fbp1bec8djsnecb6421d09a6
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var city = ""
+    @State private var state = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("\(city)")
+                    .padding()
+                TextField("Your City", text: $city)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.center)
+                    .frame(width: 200, height: 30, alignment: .center)
+                    .font(.body)
+                    .padding()
+            }
+        }
     }
+    func getJokes() {
+            let apiKey = "?rapidapi-key=(paste your Rapid API key here)"
+            let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    if json["success"] == true {
+                        let contents = json["body"].arrayValue
+                        for item in contents {
+                            let setup = item["setup"].stringValue
+                            let punchline = item["punchline"].stringValue
+                            let joke = Joke(setup: setup, punchline: punchline)
+                            jokes.append(joke)
+                        }
+                    }
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
