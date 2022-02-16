47a3ee87b27c820533e9466035e98e117dd1d301
Awhite246 <awhite@bsd220.org>
2022-02-16 12:36:00 -0600

    doing stuff, getWeather still doesn't work

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -9,42 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var city = ""
-    @State private var state = ""
+    @State private var weathers = [Weather]()
+    @State private var showingAlert = false
     var body: some View {
         NavigationView {
             VStack {
-                Text("\(city)")
+                Text("Weather")
+                    .font(.title)
+                    .fontWeight(.bold)
                     .padding()
-                TextField("Your City", text: $city)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .multilineTextAlignment(.center)
-                    .frame(width: 200, height: 30, alignment: .center)
-                    .font(.body)
-                    .padding()
+                Spacer()
             }
+            
         }
+        .onAppear {
+            getWeather()
+        }
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
     }
-    func getJokes() {
-            let apiKey = "?rapidapi-key=(paste your Rapid API key here)"
-            let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+    func getWeather() {
+            let apiKey = "rapidapi-key=cc6b0c5350msh5a3d7cf2c2a71fbp1bec8djsnecb6421d09a6"
+            let query = "https://community-open-weather-map.p.rapidapi.com/weather?q=Barrington,IL,US&units=imperial&\(apiKey)"
             if let url = URL(string: query) {
                 if let data = try? Data(contentsOf: url) {
                     let json = try! JSON(data: data)
                     if json["success"] == true {
                         let contents = json["body"].arrayValue
                         for item in contents {
-                            let setup = item["setup"].stringValue
-                            let punchline = item["punchline"].stringValue
-                            let joke = Joke(setup: setup, punchline: punchline)
-                            jokes.append(joke)
+                            let overcast = item["weather"].stringValue
+                            let main = item["main"].stringValue
+                            let weather = Weather(overcast: overcast, main: main)
+                            weathers.append(weather)
                         }
+                        return
                     }
                 }
             }
+            showingAlert = true
         }
 }
-
+struct Weather: Identifiable{
+    let id = UUID()
+    var overcast : String
+    var main : String
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
